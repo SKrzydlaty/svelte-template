cd8d2081db913a3ab27298314e7737d4faffbb53
K Stuglik <k.sin@o2.pl>
2022-02-13 10:38:28 +0100

    svelte project 01

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import Router from "svelte-spa-router";
+
+  import Footer from "./components/Footer.svelte";
+  import Header from "./components/Header.svelte";
+
+  import Contact from "./routes/Contact.svelte";
+  import Home from "./routes/Home.svelte";
+  import Gallery from "./routes/Gallery.svelte";
+  import NotFound from "./routes/NotFound.svelte";
+</script>
+
+<Tailwindcss />
+
+<div class="flex flex-col min-h-screen">
+  <Header />
+
+  <main>
+    <Router
+      routes={{
+        "/": Home,
+        "/contact": Contact,
+        "/gallery": Gallery,
+        "*": NotFound,
+      }}
+    />
+  </main>
+
+  <Footer />
+</div>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
